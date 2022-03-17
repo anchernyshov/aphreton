324f8597494776d42b45ea35cc74a716718ab3d6
anchernyshov <81495546+anchernyshov@users.noreply.github.com>
2022-03-17 21:05:40 +0300

    Add JSON Schema request validation

--- src/APIRequest.php
+++ src/APIRequest.php
@@ -5,14 +5,39 @@ namespace Aphreton;
 class APIRequest implements \JsonSerializable {
 	
 	private $post_data;
+	private $json_validator;
+	private $schema = [
+		"type" => "object",
+		"properties" => [
+			"method" => [
+				"type" => "string"
+			],
+			"action" => [
+				"type" => "string"
+			],
+			"data" => [
+				"type" => "object"
+			]
+		],
+		"required" => ["method", "action"]
+	];
 	
 	public function __construct($input) {
+		$this->json_validator = new \JsonSchema\Validator();
 		$this->post_data = json_decode($input);
 	}
 	
 	public function validate() {		
 		if (!$this->post_data) {
 			throw new \Exception('Request is empty');
+		}
+		$this->json_validator->validate($this->post_data, $this->schema, \JsonSchema\Constraints\Constraint::CHECK_MODE_NORMAL);
+		if (!$this->json_validator->isValid()) {
+			$errstr = "";
+			foreach ($this->json_validator->getErrors() as $error) {
+				$errstr .= $error['property'] . ": " . $error['message'] . "; ";
+			}
+			throw new \Exception($errstr);
 		}
 	}
 	
